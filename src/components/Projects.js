b08932a19efac3cddf4b11e0908fde355f5c3752
ChristySegunto <christysgnt@gmail.com>
2024-12-12 02:25:32 +0800

    add card for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,29 +1,68 @@
 import React, { useRef, useState } from "react";
+import ImageContentHover from 'react-image-hover';
 
-import { Row, Col, Container, Image } from 'react-bootstrap';
+import { Row, Col, Container, Image, Card } from 'react-bootstrap';
 import './../App.css';
 import './Projects.css';
 import drrms from './../images/drrms.png';
 import dentalsol from './../images/dentalsolution.png';
-import "swiper/css";
-import "swiper/css/effect-cards";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCoverflow, Pagination } from 'swiper/modules';
+import autofleet from './../images/autofleet.png';
+import portfolio from './../images/portfolio.png';
 
 const openbracket = '<';
 const closebracket = '>';
 
 function Projects({ theme, id }) {
+    const projectData = [
+        {
+          title: "Disaster Risk Reduction Management System",
+          description:
+            "A web-based system for managing Rapid Assessment of Damages Report (RADaR Files) for DEPED Central Office using Decision Tree and Neural Network Algorithm",
+          image: drrms,
+        },
+        {
+          title: "Dental Solutions",
+          description:
+            "A centralized clinic management software for Dental Solutions to store patient's data and to retrieve patient's data with Face Recognition using FaceNet Algorithm.",
+          image: dentalsol,
+        },
+        {
+          title: "AutoFleet",
+          description:
+            "AutoFleet is a comprehensive fleet management that provides real-time vehicle tracking, rental scheduling, and maintenance monitoring, ensuring efficient and seamless management of fleets of all sizes.",
+          image: autofleet,
+        },
+        {
+          title: "Portfolio Website",
+          description:
+            "A personal portfolio to showcase professional projects and achievements.",
+          image: portfolio,
+        },
+      ];
+
   return (
     <div className={`Projects ${theme}`} id={id}>
         <Row className='flex-wrap projects-row'>
-            <Col xs={12} md={6} lg={4} className={`projects-col first`}>
+            <Col xs={12} md={12} lg={4} className={`projects-col first`}>
                 <h4 className='projects-h4'>PROJECTS</h4>
             </Col>
-            <Col xs={12} md={6} lg={8} className={`projects-col second`}>
-
+            <Col xs={12} md={12} lg={8} className={`projects-col second`}>
+                <Row xs={1} md={2} className="g-4">
+                {projectData.map((project, idx) => (
+                    <Col key={idx} className="mb-4">
+                        <Card>
+                        <Card.Img variant="top" src={project.image} className="project-image"/>
+                        <Card.Body>
+                            <Card.Title className="project-title">{project.title}</Card.Title>
+                            <Card.Text className="project-text">{project.description}</Card.Text>
+                        </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+                </Row>
             </Col>
         </Row>
+        
 
         
     </div>
