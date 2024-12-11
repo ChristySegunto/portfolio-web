456f557c212a9dc8efe2197d93da1137d1ef1f66
ChristySegunto <christysgnt@gmail.com>
2024-12-12 02:25:18 +0800

    fixed responsiveness

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,15 +9,18 @@ const closebracket = '>';
 function Home({ theme, id }) {
   return (
     <div className={`Home ${theme}`} id={id}>
-      <Row className='flex-wrap justify-content-center'>
-        <Col className="home-col">
-          <div className='name-div'>
-            <h4 className='home-h4'>{openbracket}Hello!{closebracket}</h4>
-            <h1 className='home-h1'>I'M CHRISTY.</h1>
-            <p className='home-p'>Specializing in Website Design and Software Development</p>
-          </div>
+      <Row className='flex-wrap justify-content-center home-row'>
+        <Col xs={12} md={8} lg={7} className="home-col">
+
+            <div className='name-div'>
+              <h4 className='home-h4'>{openbracket}Hello!{closebracket}</h4>
+              <h1 className='home-h1'>I'M CHRISTY.</h1>
+              <p className='home-p'>Specializing in Website Design and Software Development</p>
+            </div>
+
+          
         </Col>
-        <Col className="home-col img col-5 ">
+        <Col xs={12} md={4} lg={3}className="home-col img col-5 ">
           <div className='home-image'></div>
         </Col>
       </Row>
