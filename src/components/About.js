2844d177e41b099bf5b853b51f6c877359ca6ac6
ChristySegunto <christysgnt@gmail.com>
2024-12-12 02:24:51 +0800

    add about me details

--- src/components/About.js
+++ src/components/About.js
@@ -16,8 +16,14 @@ function About({ theme, id }) {
             <Col xs={12} md={6} className={`about-col second`}>
                 <p className='about-p'>
                     Hello, I'm Christy! I am passionate about website and software development. I love building things and making innovations! I am an aspiring full stack developer from Pamantasan ng Lungsod ng Maynila.
-                    <br/><br/><br/>
-                    Hello, I'm Christy! I am passionate about website and software development. I love building things and making innovations! I am an aspiring full stack developer from Pamantasan ng Lungsod ng Maynila.
+                    <br/><br/>
+                    Fun fact about me, I often find myself fully attached in the systems I build. Each project begins with a vision—an idea that I’m determined to bring to life. But as the lines of code begin to take shape and the system starts functioning, something strange happens.
+                    <br/><br/>
+                    It’s not just about the features I’ve coded or the bug fixes I’ve tackled. It’s about the journey—the countless hours spent debugging, testing, and crying (because of frustration hahaha). Each small success, every obstacle overcome, becomes a part of me. The system stops being just lines of code; it becomes a reflection of my effort, my learning, and my growth.
+                    <br/><br/>
+                    And when the project finally finished, it feels like a piece of me is leaving. I know the system will work as it was designed, and others will use it, but I can’t help but feel a bit sad. It's like saying goodbye to a chapter of my life. It’s hard to explain to someone who doesn’t feel the same way, but I’ve realized that I don’t just build systems—I build connections with them.
+                    <br/><br/>
+                    This emotional attachment is what drives me to always do my best, to pour a bit of myself into every line of code. Even after a project is finished, I’ll look back at it and think, “This is a part of me, and it always will be.”
                 </p>
             </Col>
         </Row>
